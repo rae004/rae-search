4f177e08ae14e69ad05bc95b2ed76bbe7b5350ae
Robert Engel <robert.engel@apollidon.com>
2024-08-04 17:02:55 -0400

    Update search js and start styling page for mobile.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 console.log('app.js loaded!');
 
+function getSearchUrl(searchTerm) {
+  const encodedTerm = encodeURIComponent(searchTerm);
+  return `https://www.google.com/search?q=${encodedTerm}&udm=14`;
+}
+
 function handleSearch(event) {
   if (!navigator.onLine) {
     const searchTerm = document.getElementById('search').value;
-    const encodedTerm = encodeURIComponent(searchTerm);
-    window.location.href = `https://www.google.com/search?q=${encodedTerm}&udm=14`;
+    window.location.href = getSearchUrl(searchTerm);
   }
+}
+
+function getUdm14Info() {
+  window.location.href = getSearchUrl('What is the udm 14 parameter');
 }
 
 document.addEventListener("DOMContentLoaded", event => {
