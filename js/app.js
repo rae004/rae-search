4c1b8b99dedb39fabc8b3d3efd1992da1f85a42e
Robert Engel <robert.engel@apollidon.com>
2024-08-04 18:42:20 -0400

    Refactor mobile styles

--- js/app.js
+++ js/app.js
@@ -7,13 +7,30 @@ function getSearchUrl(searchTerm) {
 
 function handleSearch(event) {
   if (!navigator.onLine) {
+    event.preventDefault();
     const searchTerm = document.getElementById('search').value;
     window.location.href = getSearchUrl(searchTerm);
   }
 }
 
-function getUdm14Info() {
+function getUdm14Info(event) {
+  event.preventDefault();
   window.location.href = getSearchUrl('What is the udm 14 parameter');
+}
+
+function showAboutText(event) {
+  event.preventDefault();
+  const aboutTextElement = document.getElementById('about');
+  const aboutChevronElement = document.getElementById('aboutTextChevron');
+  if (aboutTextElement.style.display === 'none') {
+    aboutTextElement.style.display = 'block';
+    aboutChevronElement.style.transform = 'rotate(180deg)';
+  } else {
+    aboutTextElement.style.display = 'none';
+    aboutChevronElement.style.transform = 'rotate(0deg)';
+  }
+
+  return false;
 }
 
 document.addEventListener("DOMContentLoaded", event => {
