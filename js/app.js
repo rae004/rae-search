fd77403d784630abc601b25440a5d6b03761dbc6
Robert Engel <robert.engel@apollidon.com>
2024-08-04 15:00:15 -0400

    Setup simple no AI google search

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 console.log('app.js loaded!');
+
+function handleSearch(event) {
+  if (!navigator.onLine) {
+    const searchTerm = document.getElementById('search').value;
+    const encodedTerm = encodeURIComponent(searchTerm);
+    window.location.href = `https://www.google.com/search?q=${encodedTerm}&udm=14`;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", event => {
+  document.getElementById("search").focus();
+});
